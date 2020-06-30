03d3901fa48f803e64be9efa33a500d1b06396b9
dgaust <dgaust@outlook.com>
2020-06-30 20:05:00 +1000

    Add files via upload

---
+++ smartcover.py
@@ -0,0 +1,42 @@
+import appdaemon.plugins.hass.hassapi as hass
+import time
+
+class SmartCover(hass.Hass):
+    """Smart cover control based on state"""
+    
+    def initialize(self):  
+        self.entities = self.args["entities"]
+        self.cover = self.entities["cover"]
+        self.sensor_state = self.entities["sensor"]
+        self.light = self.entities["light"]
+        self.listen_state(self.statechange, self.sensor_state)
+        self.listen_state(self.blindpositionchange, self.cover, attribute="current_position")
+        
+    def statechange(self, entity, attribute, old, new, kwargs):
+        self.log(new)
+        self.coverstate = self.get_state(self.cover)
+        self.coverposition = self.get_state(self.cover, attribute="current_position")
+        self.log("Position: " + str(self.coverposition))
+        if new == "Key Held down" or new == "14":
+           self.log(self.coverstate)
+           self.call_service("light/toggle", entity_id = self.light)
+           if self.coverstate == "opening" or self.coverstate == "closing":
+              self.call_service("cover/stop_cover", entity_id=self.cover)
+              self.log("Stopping Blind")
+           elif self.coverstate == "closed":
+              self.call_service("cover/open_cover", entity_id=self.cover)
+              self.log("Opening Blind")
+           elif self.coverposition > 50:
+              self.call_service("cover/close_cover", entity_id=self.cover)
+              self.log("Closing Blind")
+           else:
+              self.call_service("cover/open_cover", entity_id=self.cover)
+              self.log("Opening Blind")
+    
+    def blindpositionchange(self, entity, attribute, old, new, kwargs):
+        self.log(new)
+        if new == 100 or new == 0:
+           self.call_service("cover/stop_cover", entity_id=self.cover)
+           self.log("Stopping Cover")
+    
+        
